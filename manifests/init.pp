f6ce2aa7c93e12719212acb7056401fd17cb78f1
olmectech <olmectech1@gmail.com>
2014-09-10 21:25:29 -0400

    first commit

---
+++ manifests/init.pp
@@ -0,0 +1,14 @@
+class sshkey ($user_data = hiera('data') ) {
+
+  $new_name = $user_data[0]['name']
+  $key_new = $user_data[1]['key']
+  $hostname_new = $user_data[2]['hostname']
+  $type_new = $user_data[3]['type']
+  $ssh_name = $user_data[4]['sshname']
+
+ssh_authorized_key { $ssh_name:
+  user => $new_name,
+  type => $type_new,
+  key => $key_new
+ }
+}
